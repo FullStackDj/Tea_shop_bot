cbb20cc4d670802eb3de93e8daa827238d828cc0
FullStackDj <full.stackdj@gmail.com>
2025-03-18 19:03:30 +0200

    Implemented command handlers and added buttons

--- app.py
+++ app.py
@@ -1,27 +1,27 @@
+import asyncio
 import environ
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
-import asyncio
+from aiogram.types import BotCommandScopeAllPrivateChats
+
+from handlers.user_private import user_private_router
+from common.bot_commands_list import private
 
 env = environ.Env()
 environ.Env.read_env()
 
+ALLOWED_UPDATES = ['message, edited_message']
+
 bot = Bot(token=env('TOKEN'))
 dp = Dispatcher()
 
-
-@dp.message(CommandStart())
-async def start_cmd(message: types.Message):
-    await message.answer('Start')
-
-
-@dp.message()
-async def echo(message: types.Message):
-    await message.answer(message.text)
+dp.include_router(user_private_router)
 
 
 async def main():
-    await dp.start_polling(bot)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await bot.set_my_commands(commands=private, scope=BotCommandScopeAllPrivateChats())
+    await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
 
 
 asyncio.run(main())
---
+++ handlers/user_private.py
@@ -0,0 +1,32 @@
+from aiogram import types, Router, F
+from aiogram.filters import CommandStart, Command, or_f
+
+user_private_router = Router()
+
+
+@user_private_router.message(CommandStart())
+async def start_cmd(message: types.Message):
+    await message.answer('12345')
+
+
+@user_private_router.message(or_f(Command('menu')))
+async def menu_cmd(message: types.Message):
+    await message.answer('menu:')
+
+
+@user_private_router.message(F.text.lower() == 'about us')
+@user_private_router.message(Command('about'))
+async def menu_cmd(message: types.Message):
+    await message.answer('about:')
+
+
+@user_private_router.message(F.text.lower() == 'payment options')
+@user_private_router.message(Command('payment'))
+async def menu_cmd(message: types.Message):
+    await message.answer('payment:')
+
+
+@user_private_router.message(F.text.lower().contains('deliver') | (F.text.lower() == 'delivery options'))
+@user_private_router.message(Command('shipping'))
+async def menu_cmd(message: types.Message):
+    await message.answer('shipping and delivery:')
